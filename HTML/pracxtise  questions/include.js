7ce10ff306dfa0a71c7bae63351a4b26f80c1c14
Paritosh Pradhan <148734120+pari1807@users.noreply.github.com>
2025-01-31 16:24:14 +0530

    Html and css

---
+++ HTML/pracxtise  questions/include.js
@@ -0,0 +1,12 @@
+
+// var fullName="Paritosh Pradhan";
+// var age = 30;
+// console.log(fullName);
+
+/*const product={
+    title:"Laptop",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+console.log(typeof product["rating"]);*/
