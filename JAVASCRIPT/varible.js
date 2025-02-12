5555005a292f3740e488ee460fc318eb857ed170
Paritosh Pradhan <148734120+pari1807@users.noreply.github.com>
2025-02-12 17:07:01 +0530

    Learned more about CSS and JAVASCRIPT

---
+++ JAVASCRIPT/varible.js
@@ -0,0 +1,22 @@
+var age = 25;
+// a sample how a var works 
+if(age){
+    console.log("Age is defined");
+}
+//sample of functions 
+function solve(){
+    var age = 25;
+    console.log("Age is defined");
+}
+console.log(age);
+solve();
+{
+let a = 10;
+console.log(a);
+}
+
+const a = 25;
+console.log(a);
+//redeclaring any const is not possible in javascript 
+//const a = 20;
+console.log(a);
