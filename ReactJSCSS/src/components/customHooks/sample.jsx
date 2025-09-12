493d27224005705b9943158dbeb027d9dead8aa6
Piyush610 <148734120+pari1807@users.noreply.github.com>
2025-09-12 16:01:58 +0530

    Added basic Projects to the folder

---
+++ ReactJSCSS/src/components/customHooks/sample.jsx
@@ -0,0 +1,24 @@
+// import {useEffect,useState} from " react";
+
+// const sample = () => {
+//     const [data, setData] = useState(null);
+
+//     useEffect(()=>{
+//         fetch('https://jsonplaceholder.typicode.com/todos')
+//         .then((res) => res.json())
+//         .then((data) => setData(data));
+//     },[]);
+
+//     return (
+//         <div>
+//            {data && data.map((item) => {
+//             return <p key={item.id}>{item.title}</p>
+//            })} 
+//         </div>
+//     );
+// };
+
+// export default sample;
+
+
+//useId Hooks 
