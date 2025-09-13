09b9a604a670ce1ea7bd177e9cb55f8462ba19dd
Piyush610 <148734120+pari1807@users.noreply.github.com>
2025-09-13 22:34:15 +0530

    building basic projects

---
+++ ProjectBasics/src/Components/Counter/Counter.jsx
@@ -0,0 +1,17 @@
+import  './index.css';
+import {useState} from "react";
+
+const Counter = () => {
+
+    const [count,setCount] = useState(0);
+    
+
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Counter;
+
